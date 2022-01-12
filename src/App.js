0a0e8e84dcead8320223a9410d5b9e33e9222394
Yordan <yordan_yordanov001@abv.bg>
2022-01-12 17:08:16 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handle = () => {
+    localStorage.setItem("Name", name);
+  };
+  const remove = () => {
+    localStorage.removeItem("Name");
+  };
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea
+              className="textarea is-large"
+              placeholder="Notes..."
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button
+          className="button is-large is-primary is-active"
+          onClick={handle}
+        >
+          Save
+        </button>
+        <button className="button is-large" onClick={remove}>
+          Clear
+        </button>
       </div>
     </div>
   );
